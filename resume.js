e864667e517a55bebc322c0b70a47002a0c928bd
Rakshana <142417874+Rakshana77@users.noreply.github.com>
2023-10-26 04:38:34 -0700

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,39 @@
+{
+  "personal_info": {
+    "name": "Rakshana",
+    "address": "47,kollam pattarai street,nellpettai,madurai",
+    "phone": "8668139762",
+    "email": "sonarakshana@gmail.com"
+  },
+  "summary": "i am a software developer with a passion for creating innovative web applications. Strong problem-solving skills and a proven track record of delivering high-quality code on time.",
+  "education": [
+    {
+      "degree": "BE-electronics and communication engineering",
+      "university": "sethu institute of Technology",
+      "graduation_date": "May 2021"
+    }
+  ],
+        "work_experience": {
+        "fresher"
+    },
+    "projects": [
+        {
+            "name": "bone mineral density finding device",
+            "summary": "to find the T-score value from the x-rays of bone then this device is find out the person has osteoporosis or not"
+        "duration": "6 months".
+        }
+        {
+            "name": "osteoarthiritis",
+            "summary": "to find the value of the liquid in between the knee from the x-rays then this device is find out the person has osteoarthiritis or not"
+        "duration": "6 months"
+        "technology":"python"
+        }
+  ]
+  "skills": {
+    "programming_languages": ["JavaScript", "Python", "c"],
+    "web_technologies": ["HTML", "CSS", "React", "Node.js"],
+    "tools": ["Git", "Visual Studio Code", ],
+    "soft_skills": ["Problem Solving", "Teamwork", "Communication"]
+  },
+  "languages": ["English (Fluent)"]
+}
